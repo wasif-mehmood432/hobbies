d206d699b1e38d97854d39a8b87ed6403a2064e5
wasif-mehmood432 <wasifmehmood238@gmail.com>
2025-07-23 13:12:11 +0500

    Reconnenect and responsive

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,15 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/react-test/' : '/', // 👈 ADD THIS LINE
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
